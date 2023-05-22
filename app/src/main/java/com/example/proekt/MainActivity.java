0a9a79f2850eeb433d03fe0c01895fa56db5ff07
maksi_000 <maksimov.i289@.com>
2023-05-22 18:34:09 +0300

    Initial commit

---
+++ app/src/main/java/com/example/proekt/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.proekt;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+
+    private TabLayout tabLayout;
+    private TabsAdapter tabsAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.main_tabs_pager);
+        tabsAdapter = new TabsAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabsAdapter);
+
+        tabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
---
+++ app/src/main/java/com/example/proekt/TabsAdapter.java
@@ -0,0 +1,46 @@
+package com.example.proekt;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class TabsAdapter extends FragmentPagerAdapter {
+    public TabsAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                ProfFragment profFragment = new ProfFragment();
+                return profFragment;
+            case 1:
+                BasaFragment basaFragment = new BasaFragment();
+                return basaFragment;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Профиль";
+            case 1:
+                return "База";
+            default:
+                return null;
+        }
+    }
+}
