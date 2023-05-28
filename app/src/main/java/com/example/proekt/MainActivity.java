06dd9b15827ef766242d525cf467fef26bd58a3c
maksi_000 <maksimov.i289@.com>
2023-05-28 18:32:26 +0300

    Initial commit

--- app/src/main/java/com/example/proekt/MainActivity.java
+++ app/src/main/java/com/example/proekt/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.proekt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     private TabLayout tabLayout;
     private TabsAdapter tabsAdapter;
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
 
 
     @Override
@@ -24,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
 
         viewPager = (ViewPager) findViewById(R.id.main_tabs_pager);
         tabsAdapter = new TabsAdapter(getSupportFragmentManager());
@@ -31,6 +40,31 @@ public class MainActivity extends AppCompatActivity {
 
         tabLayout = (TabLayout) findViewById(R.id.main_tabs);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.func_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.exit_menu) {
+            Toast.makeText(this, "Выход", Toast.LENGTH_SHORT).show();
+            mAuth.signOut();
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        return true;
+
     }
 
 
