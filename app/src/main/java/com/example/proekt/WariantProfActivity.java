b2cc216902f0cae09ccc299ccfc0935bc96e999a
maksi_000 <maksimov.i289@.com>
2023-06-03 23:54:05 +0300

    Initial commit

--- app/src/main/java/com/example/proekt/WariantProfActivity.java
+++ app/src/main/java/com/example/proekt/WariantProfActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class WariantProfActivity extends AppCompatActivity implements View.OnClickListener {
@@ -15,6 +16,8 @@ public class WariantProfActivity extends AppCompatActivity implements View.OnCli
         setContentView(R.layout.activity_wariant_prof);
         TextView textView = findViewById(R.id.w1);
         textView.setOnClickListener(this);
+        ImageView imageView = findViewById(R.id.imageView_chat);
+        imageView.setOnClickListener(this);
     }
 
     @Override
@@ -24,6 +27,11 @@ public class WariantProfActivity extends AppCompatActivity implements View.OnCli
                 Intent intent = new Intent(WariantProfActivity.this, WRActivity.class);
                 intent.putExtra("wariant_number", "1");
                 startActivity(intent);
+                break;
+            case R.id.imageView_chat:
+                Intent intent_chat = new Intent(WariantProfActivity.this, ChatActivity.class);
+                intent_chat.putExtra("chat", "pw1");
+                startActivity(intent_chat);
                 break;
         }
     }
