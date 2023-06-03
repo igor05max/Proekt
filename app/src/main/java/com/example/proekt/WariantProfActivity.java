fa03ad23b4e30e5e5235eb0c7a1bfef42ba5b2a3
maksi_000 <maksimov.i289@.com>
2023-06-03 15:38:03 +0300

    Initial commit

--- app/src/main/java/com/example/proekt/WariantProfActivity.java
+++ app/src/main/java/com/example/proekt/WariantProfActivity.java
@@ -3,12 +3,27 @@ package com.example.proekt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class WariantProfActivity extends AppCompatActivity {
+public class WariantProfActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wariant_prof);
+        TextView textView = findViewById(R.id.w1);
+        textView.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.w1:
+
+                break;
+        }
+    }
+
+
 }
